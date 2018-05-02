2db27c4a138211398bee34dc347e72d554ba6998
Cameron Alexander <emptyflash@gmail.com>
2018-05-01 20:04:27 -0500

    new tests wip

---
+++ tests/duplicate_timestamps_test.go
@@ -0,0 +1,32 @@
+package tests
+
+import (
+	"context"
+	"gopkg.in/BTrDB/btrdb.v4"
+	"testing"
+)
+
+func TestInsertDuplciateTimestampsErrors(t *testing.T) {
+	ctx := context.Background()
+	db := helperConnect(t, ctx)
+
+	firstStream := helperCreateDefaultStream(t, ctx, db, nil, nil)
+	secondStream := helperCreateDefaultStream(t, ctx, db, nil, nil)
+	start := int64(1524520800000)
+	normalData := helperRandomDataCount(start, start+100, 100)
+
+	err := firstStream.Insert(ctx, normalData)
+	if err != nil {
+		t.Fatalf("Error inserting normal data: %v", err)
+	}
+
+	duplicateData := make([]btrdb.RawPoint, 100000)
+	for i, _ := range duplicateData {
+		point := btrdb.RawPoint{start, 555}
+		duplicateData[i] = point
+	}
+	err = secondStream.Insert(ctx, duplicateData)
+	if err != nil {
+		t.Fatalf("Error inserting normal data: %v", err)
+	}
+}
