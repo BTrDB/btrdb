30d65c05bf2d9336d93d583aca8e8147a4f7f0f7
cameron <emptyflash@gmail.com>
2018-05-05 10:40:31 -0500

    fix randomly failing test and add query after insert

--- tests/duplicate_timestamps_test.go
+++ tests/duplicate_timestamps_test.go
@@ -14,14 +14,15 @@ func TestInsertDuplciateTimestampsErrors(t *testing.T) {
 	firstStream := helperCreateDefaultStream(t, ctx, db, nil, nil)
 	secondStream := helperCreateDefaultStream(t, ctx, db, nil, nil)
 	start := int64(1524520800000)
-	normalData := helperRandomDataCount(start, start+100, 100)
+	end := start + 100
+	normalData := helperRandomDataCount(start, end, 100)
 
 	err := firstStream.Insert(ctx, normalData)
 	if err != nil {
 		t.Fatalf("Error inserting normal data: %v", err)
 	}
 
-	duplicateData := make([]btrdb.RawPoint, 50000)
+	duplicateData := make([]btrdb.RawPoint, 1000)
 	for i, _ := range duplicateData {
 		point := btrdb.RawPoint{start, 555}
 		duplicateData[i] = point
@@ -29,6 +30,20 @@ func TestInsertDuplciateTimestampsErrors(t *testing.T) {
 	timedCtx, _ := context.WithTimeout(ctx, 1*time.Second)
 	err = secondStream.Insert(timedCtx, duplicateData)
 	if err != nil {
-		t.Fatalf("Error inserting normal data: %v", err)
+		t.Fatalf("Error inserting duplicate timestamp data: %v", err)
+	}
+
+	pc, _, errc := firstStream.RawValues(ctx, start, end, 0)
+	err = <-errc
+	if err != nil {
+		t.Fatalf("Failed querying first stream after duplicate insert: %v", err)
+	}
+
+	rv := make([]btrdb.RawPoint, 0)
+	for p := range pc {
+		rv = append(rv, p)
+	}
+	if len(rv) != len(normalData) {
+		t.Fatal("Queried data len was different from original data len")
 	}
 }
