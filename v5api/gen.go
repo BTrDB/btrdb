6383f095d6fd080999081658ee4193b6a54b14c4
Jacob Moody <moody@posixcafe.org>
2021-09-22 12:24:23 -0600

    regenerate grpc-gw code as well

--- v5api/gen.go
+++ v5api/gen.go
@@ -1,3 +1,5 @@
 package v5api
 
-//go:generate protoc -I . -I /usr/local/include -I ./include --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative btrdb.proto
+//go:generate protoc -I /usr/local/include -I ./include -I . --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative btrdb.proto
+//go:generate protoc -I /usr/local/include -I ./include -I . --grpc-gateway_out=logtostderr=true:. --grpc-gateway_opt=paths=source_relative btrdb.proto
+// //go:generate protoc -I/usr/local/include -I ./include -I. --openapiv2_out=logtostderr=true:. btrdb.proto
